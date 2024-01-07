5ede946409533ab59d2b146fc2a28b305c9c0a72
Zmachinskii Dmitrii <zmachinskiiof@gmail.com>
2024-01-07 20:21:02 +0200

    Reformating

--- src/main.php
+++ src/main.php
@@ -1,68 +1,51 @@
 <?php
-function bracketStringAsessment(string $bracketExpression):bool
+function bracketStringAsessment(string $bracketExpression): bool
 {
-    $stack=[];
-    $openingBracketString="([<{";
-    foreach (str_split($bracketExpression) as $char)
-    {
-    if(str_contains($openingBracketString, $char))
-    {
-    $stack[] = $char;
-    }
-    else
-    {
-        switch($char)
-        {
-            case(")"):
-                if(end($stack)==="(")
-                {
-                    array_pop($stack);
+    $stack = [];
+    $openingBracketString = "([<{";
+    foreach (str_split($bracketExpression) as $char) {
+        if (str_contains($openingBracketString, $char)) {
+            $stack[] = $char;
+        } else {
+            switch ($char) {
+                case(")"):
+                    if (end($stack) === "(") {
+                        array_pop($stack);
 
-                }
-                else
-                {
-                    return false;
-                }
-                break;
-            case("]"):
-                if(end($stack)==="[")
-                {
-                    array_pop($stack);
-                }
-                else
-                {
-                    return false;
-                }
-               break;
-            case("}"):
-                if(end($stack)==="{")
-                {
-                    array_pop($stack);
-                }
-                else
-                {
-                    return false;
-                }
-               break;
-            case(">"):
-                if(end($stack)==="<")
-                {
-                    array_pop($stack);
-                }
-                else
-                {
-                    return false;
-                }
-                break;
-            default:
-                continue 2;
+                    } else {
+                        return false;
+                    }
+                    break;
+                case("]"):
+                    if (end($stack) === "[") {
+                        array_pop($stack);
+                    } else {
+                        return false;
+                    }
+                    break;
+                case("}"):
+                    if (end($stack) === "{") {
+                        array_pop($stack);
+                    } else {
+                        return false;
+                    }
+                    break;
+                case(">"):
+                    if (end($stack) === "<") {
+                        array_pop($stack);
+                    } else {
+                        return false;
+                    }
+                    break;
+                default:
+                    continue 2;
+            }
         }
     }
-    }
-    if($stack!==[])
-    {
+    if ($stack !== []) {
         return false;
     }
     return true;
 }
+
 echo(bracketStringAsessment("[]<><>{{<{}>}}test()(())"));
--- tests/BracketTest.php
+++ tests/BracketTest.php
@@ -6,29 +6,33 @@ use PHPUnit\Framework\TestCase;
 
 class BracketTest extends TestCase
 {
-    public function testGoodBrackets():void
-    {$this->assertEquals(true,bracketStringAsessment("()[]{}<>"));
-        $this->assertEquals(true,bracketStringAsessment("{()}<()>()"));
-        $this->assertEquals(true,bracketStringAsessment("dd(){}{{()}}(fffff)()"));
-    }
-    public function testBadBracketsOpeningBracketCase():void
+    public function testGoodBrackets(): void
     {
-        $this->assertEquals(false,bracketStringAsessment("("));
-        $this->assertEquals(false,bracketStringAsessment("["));
-        $this->assertEquals(false,bracketStringAsessment("{"));
-        $this->assertEquals(false,bracketStringAsessment("<"));
+        $this->assertEquals(true, bracketStringAsessment("()[]{}<>"));
+        $this->assertEquals(true, bracketStringAsessment("{()}<()>()"));
+        $this->assertEquals(true, bracketStringAsessment("dd(){}{{()}}(fffff)()"));
     }
-    public function testBadBracketsClosingBracketCase():void
+
+    public function testBadBracketsOpeningBracketCase(): void
     {
-        $this->assertEquals(false,bracketStringAsessment(")"));
-        $this->assertEquals(false,bracketStringAsessment("]"));
-        $this->assertEquals(false,bracketStringAsessment("}"));
-        $this->assertEquals(false,bracketStringAsessment(">"));
+        $this->assertEquals(false, bracketStringAsessment("("));
+        $this->assertEquals(false, bracketStringAsessment("["));
+        $this->assertEquals(false, bracketStringAsessment("{"));
+        $this->assertEquals(false, bracketStringAsessment("<"));
     }
-    public function testBadBracketsWrongOrderCase():void
+
+    public function testBadBracketsClosingBracketCase(): void
     {
-        $this->assertEquals(false,bracketStringAsessment("({)}"));
-        $this->assertEquals(false,bracketStringAsessment("[<]>"));
-        $this->assertEquals(false,bracketStringAsessment("(<)>"));
+        $this->assertEquals(false, bracketStringAsessment(")"));
+        $this->assertEquals(false, bracketStringAsessment("]"));
+        $this->assertEquals(false, bracketStringAsessment("}"));
+        $this->assertEquals(false, bracketStringAsessment(">"));
+    }
+
+    public function testBadBracketsWrongOrderCase(): void
+    {
+        $this->assertEquals(false, bracketStringAsessment("({)}"));
+        $this->assertEquals(false, bracketStringAsessment("[<]>"));
+        $this->assertEquals(false, bracketStringAsessment("(<)>"));
     }
 }
